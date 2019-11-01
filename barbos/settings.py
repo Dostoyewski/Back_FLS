603f965bfa83fa39dc41a684ebabbb603c548579
Dostoyewski <dostoyewski@yandex.ru>
2019-11-01 21:39:53 +0300

    changed db settings

--- barbos/settings.py
+++ barbos/settings.py
@@ -80,7 +80,12 @@ WSGI_APPLICATION = 'barbos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
